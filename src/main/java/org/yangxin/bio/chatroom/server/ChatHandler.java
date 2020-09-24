6d68c294a5ecf04d1feb96a3b7f4fa7b17f7f4b5
yangxin <1393003255@qq.com>
2020-09-24 19:43:23 +0800

    feat: 实现聊天室监听器：ChatHandler（上）

--- src/main/java/org/yangxin/bio/chatroom/server/ChatHandler.java
+++ src/main/java/org/yangxin/bio/chatroom/server/ChatHandler.java
@@ -1,8 +1,27 @@
 package org.yangxin.bio.chatroom.server;
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * @author yangxin
  * 2020/09/24 16:48
  */
-public class ChatHandler {
+public class ChatHandler implements Runnable {
+
+    private ChatServer server;
+    private Socket socket;
+
+    public ChatHandler(ChatServer server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            server.addClient(socket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
