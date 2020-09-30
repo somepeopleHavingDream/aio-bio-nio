baec2e7ef58a1ba71c12acd671079ca510b95d3f
yangxin <1393003255@qq.com>
2020-09-30 17:49:31 +0800

    feat: 服务器端实现（第三回合）

--- src/main/java/org/yangxin/aio/Server.java
+++ src/main/java/org/yangxin/aio/Server.java
@@ -3,9 +3,12 @@ package org.yangxin.aio;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author yangxin
@@ -53,11 +56,56 @@ public class Server {
      * @author yangxin
      * 2020/09/30 14:51
      */
-    private static class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Object> {
+    private class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Object> {
 
         @Override
         public void completed(AsynchronousSocketChannel asynchronousSocketChannel, Object o) {
+            if (serverChannel.isOpen()) {
+                serverChannel.accept(null, this);
+            }
 
+            AsynchronousSocketChannel clientChannel = asynchronousSocketChannel;
+            if (clientChannel != null && clientChannel.isOpen()) {
+                ClientHandler handler = new ClientHandler(clientChannel);
+
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                Map<String, Object> info = new HashMap<>();
+                info.put("type", "read");
+                info.put("buffer", buffer);
+
+                clientChannel.read(buffer, info, handler);
+            }
+        }
+
+        @Override
+        public void failed(Throwable throwable, Object o) {
+            // 处理错误
+        }
+    }
+
+    /**
+     * @author yangxin
+     * 2020/09/30 17:38
+     */
+    private class ClientHandler implements CompletionHandler<Integer, Object> {
+
+        private final AsynchronousSocketChannel clientChannel;
+
+        public ClientHandler(AsynchronousSocketChannel channel) {
+            this.clientChannel = channel;
+        }
+
+        @Override
+        public void completed(Integer integer, Object o) {
+            Map<String, Object> info = (Map<String, Object>) o;
+            String type = (String) info.get("type");
+
+            if ("read".equals(type)) {
+                ByteBuffer buffer = (ByteBuffer) info.get("buffer");
+                buffer.flip();
+                info.put("type", "write");
+                clientChannel.write(buffer, info, this);
+            }
         }
 
         @Override
